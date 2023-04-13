fc1216f55bd28deffaa226d106e377b766c9a955
Zhaniya116 <105491560+Zhaniya116@users.noreply.github.com>
2023-04-13 22:32:42 +0600

    Add files via upload

---
+++ primeapp/main_test.go
@@ -0,0 +1,107 @@
+package main
+
+import (
+	"bufio"
+	"io"
+	"os"
+	"strings"
+	"testing"
+)
+
+func Test_isPrime(t *testing.T) {
+	primeTests := []struct {
+		name     string
+		testNum  int
+		expected bool
+		msg      string
+	}{
+		{"prime", 7, true, "7 is a prime number!"},
+		{"not prime", 8, false, "8 is not a prime number because it is divisible by 2!"},
+		{"zero", 0, false, "0 is not prime, by definition!"},
+		{"one", 1, false, "1 is not prime, by definition!"},
+		{"negative number", -11, false, "Negative numbers are not prime, by definition!"},
+	}
+
+	for _, e := range primeTests {
+		result, msg := isPrime(e.testNum)
+		if e.expected && !result {
+			t.Errorf("%s: expected true but got false", e.name)
+		}
+
+		if !e.expected && result {
+			t.Errorf("%s: expected false but got true", e.name)
+		}
+
+		if e.msg != msg {
+			t.Errorf("%s: expected %s but got %s", e.name, e.msg, msg)
+		}
+	}
+}
+
+func Test_prompt(t *testing.T) {
+
+	oldOut := os.Stdout
+
+	r, w, err := os.Pipe()
+	if err != nil {
+		t.Fatalf("Could create a pipe %v", err.Error())
+	}
+
+	os.Stdout = w
+
+	prompt()
+
+	_ = w.Close()
+
+	os.Stdout = oldOut
+
+	out, _ := io.ReadAll(r)
+
+	if string(out) != "-> " {
+		t.Errorf("incorrect promt: expected -> but got %s", string(out))
+	}
+}
+
+func Test_intro(t *testing.T) {
+	oldOut := os.Stdout
+	r, w, _ := os.Pipe()
+	os.Stdout = w
+
+	intro()
+
+	_ = w.Close()
+	out, _ := io.ReadAll(r)
+
+	os.Stdout = oldOut
+
+	expectedOutput := "Is it Prime?\n------------\nEnter a whole number, and we'll tell you if it is a prime number or not. Enter q to quit.\n-> "
+
+	if string(out) != expectedOutput {
+		t.Errorf("incorrect output: expected %q but got %q", expectedOutput, string(out))
+	}
+}
+
+func Test_checkNumbers(t *testing.T) {
+	tests := []struct {
+		name     string
+		input    string
+		expected string
+	}{
+		{"valid number", "7\n", "7 is a prime number!"},
+		{"not a number", "hello\n", "Please enter a whole number!"},
+		{"negative number", "-11\n", "Negative numbers are not prime, by definition!"},
+		{"zero", "0\n", "0 is not prime, by definition!"},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			r := strings.NewReader(tt.input)
+			scanner := bufio.NewScanner(r)
+			result, _ := checkNumbers(scanner)
+
+			if result != tt.expected {
+				t.Errorf("expected '%s' but got '%s'", tt.expected, result)
+			}
+		})
+	}
+}
